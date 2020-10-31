65b9d3067eec0704e883a498463ee29896030da8
ArkadiyChernetskiy <70897621+ArkadiyChernetskiy@users.noreply.github.com>
2020-10-31 23:46:43 +0300

    Update zadachkaA.cpp
    Struct Node можно было сохранить внутри Stack в private, так как она только там используется. В остальном всё хорошо, только по мелочи - после окончания функции ; не нужна и абзацы ставь между логическими блоками. Принято

--- zadachkaA.cpp
+++ zadachkaA.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
+
+
 struct Node{
 	int value;
 	Node* previous;
 	Node() : value(0), previous(nullptr) {}
 };
+
 class Stack {
 private:
 	size_t size;
 	Node* last;
 public:
-	Stack() : size(0), last(nullptr) {};
+	Stack() : size(0), last(nullptr) {}
+	
 	size_t length() {
 		return size;
-	};
+	}
+	
 	void push(int x) {
 		Node* next = new Node();
 		next->value = x;
@@ -21,9 +26,11 @@ public:
 		++size;
 		last = next;
 	}
+	
 	int back() {
 		return last->value;
 	}
+	
 	int pop() {
 		int result = last->value;
 		Node* copy = last;
@@ -32,6 +39,7 @@ public:
 		--size;
 		return result;
 	}
+	
 	void clear() {
 		while (size > 0) {
 			pop();
