950bafec5ce67334babe751579a4e2d81532a950
Kseniia Altynbaeva <altynbaeva.kr@phystech.edu>
2020-11-29 23:19:10 +0300

    99936357

--- goblins.cpp
+++ goblins.cpp
@@ -4,43 +4,51 @@
 using std::cout;
 using std::cin;
 using std::deque;
-
+void adding(int number, deque <int>& qend) {
+	qend.push_back(number);
+}
+void priveleged(int number, deque<int>& qbegin, deque<int>& qend) {
+	while (qbegin.size() < qend.size()) {
+		qbegin.push_back(qend.front());
+		qend.pop_front();
+	}
+	qbegin.push_back(number);
+	while (qbegin.size() > qend.size() + 1) {
+		qend.push_front(qbegin.back());
+		qbegin.pop_back();
+	}
+}
+void toshamans(deque<int>& qbegin, deque<int>& qend) {
+	if (qbegin.empty()) {
+		cout << qend.front() << std::endl;
+		qend.pop_front();
+	}
+	else {
+		cout << qbegin.front() << std::endl;
+		qbegin.pop_front();
+	}
+}
 int main() {
 	std::ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
-	cin >> n;
+	int amountOfrequests;
+	cin >> amountOfrequests;
 	deque <int> qend;
 	deque <int> qbegin;
-	for (int j = 0; j < n; ++j) {
-		char k;
-		int i;
-		cin >> k;
-		if (k == '+') {
-			cin >> i;
-			qend.push_back(i);
+	for (int j = 0; j < amountOfrequests; ++j) {
+		char request;
+		int number;
+		cin >> request;
+		if (request == '+') {
+			cin >> number;
+			adding(number, qend);
 		}
-		else if (k == '-') {
-			if (qbegin.empty()) {
-				cout << qend.front() << std::endl;
-				qend.pop_front();
-			}
-			else {
-				cout << qbegin.front() << std::endl;
-				qbegin.pop_front();
-			}
+		else if (request == '-') {
+			toshamans(qbegin, qend);
 		}
 		else {
-			cin >> i;
-			while (qbegin.size() < qend.size()) {
-				qbegin.push_back(qend.front());
-				qend.pop_front();
-			}
-			qbegin.push_back(i);
-			while (qbegin.size() > qend.size() + 1) {
-				qend.push_front(qbegin.back());
-				qbegin.pop_back();
-			}
+			cin >> number;
+			priveleged(number, qbegin, qend);
 		}
 	}
 	return 0;
