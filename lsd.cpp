670b11f687711920a14365d6b53288da9fb96f7e
KseniiaAltynbaeva <70897569+KseniiaAltynbaeva@users.noreply.github.com>
2020-12-19 18:37:31 +0300

    101779193

--- lsd.cpp
+++ lsd.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 #include <string>
 #include <cmath>
-
+ 
 void stableSort(std::vector <long long>& input, int rank) {
-	std::vector <int> count(10, 0);
+	std::vector <int> count(255, 0);
 	for (int i = 0; i < input.size(); ++i) {
-		count[input[i] / (long long)pow(10, rank) % 10]++;
+		count[input[i] / (long long)pow(255, rank) % 255]++;
 	}
 	for (int i = 1; i < count.size(); ++i) {
 		count[i] += count[i - 1];
 	}
 	std::vector <long long> ans(input);
 	for (int i = input.size() - 1; i >= 0; --i) {
-		input[--count[ans[i] / (long long)pow(10, rank) % 10]] = ans[i];
+		input[--count[ans[i] / (long long)pow(255, rank) % 255]] = ans[i];
 	}
 }
 void LSD(std::vector <long long>& input, long long maxsize) {
 	int  n = 0;
 	while (maxsize > 0) {
-		maxsize /= 10;
+		maxsize >>= 8;
 		++n;
 	}
 	for (int i = 0; i < n; ++i) {
@@ -46,5 +46,6 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		std::cout << input[i] << " ";
 	}
+	//system("pause");
 	return 0;
 }
