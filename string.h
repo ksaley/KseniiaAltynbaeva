4437fac0c8a29d484e4a72e53465935ab03cd872
KseniiaAltynbaeva <70897569+KseniiaAltynbaeva@users.noreply.github.com>
2021-02-01 18:11:32 +0500

    Update string.h

--- string.h
+++ string.h
@@ -24,14 +24,14 @@ private:
         delete[] copy;
     }
     String(size_t sz) : sz(sz), capacity(sz), str(new char[sz]) {
-        memset(str, '\0', sz);// не обязательно, всё равно как понимаю ты будешь эту память перезаписывать
+        memset(str, '\0', sz);
     }
 public:
     String() = default;
-    String(const char filling):sz(1), capacity(1), str(new char[1]){// Почему бы заранее не создавать массив в 2 раза больше, чтобы потом сразу не добавлять?
+    String(const char filling):sz(1), capacity(1), str(new char[1]){
         memset(str, filling, sz);
     }
-    String(size_t sz, char filling) : sz(sz), capacity(sz), str(new char[sz]) {// Аналогично
+    String(size_t sz, char filling) : sz(sz), capacity(sz), str(new char[sz]) {
         memset(str, filling, sz);
     }
 
@@ -66,7 +66,7 @@ public:
         str[sz] = back;
         ++sz;
     }
-    
+
     void pop_back() {
         if (sz - 1 <= capacity/4) {
             change_capacity_down();
@@ -87,14 +87,14 @@ public:
         return str[i];
     }
     bool operator==(const String& s) {
-        bool flag = true;// elements_is_equal
+        bool elements_are_equal = true;
         size_t index = 0;
         if (sz != s.sz) return false;
-        while (flag && index < sz) {
-            flag = (str[index] == s.str[index]);
+        while (elements_are_equal && index < sz) {
+            elements_are_equal = (str[index] == s.str[index]);
             ++index;
         }
-        return flag;
+        return elements_are_equal;
     }
 
     char& front() {
@@ -116,9 +116,7 @@ public:
         return newstring;
     }
     void clear() {
-        while (sz> 0) {// n раз sz--? Зачем? Проще ведь сразу просто написать sz = 0, даже можно изменить размер на меньший, но не обязательно
-            pop_back();
-        }
+        sz = 0;
     }
 
     friend std::ostream& operator<<(std::ostream& stream, const String& s) {
@@ -143,13 +141,17 @@ public:
         return stream;
     }
     String& operator+=(const String& s) {
-        size_t current = sz + s.sz;// А почему бы не провести проверку, вдруг у нас уже хватает места для добавления?
-        char* copy = new char[current];
-        memcpy(copy,str, sz);
-        memcpy(copy + sz, s.str, s.sz);
-        std::swap(copy,str);
-        delete[] copy;
+        if (capacity < sz + s.sz) {
+            size_t current = sz + s.sz;
+            char *copy = new char[current];
+            memcpy(copy, str, sz);
+            memcpy(copy + sz, s.str, s.sz);
+            std::swap(copy, str);
+            delete[] copy;
+        }
         sz+= s.sz;// Кстати, у тебя не изменяется capacity, что может потом аукнуться, когда будешь pop делать
+        // разве capacity не изменяется в if, когда мы создаем new char[current], иначе capacity достаточно,
+        //мы ведь увеличиваем, когда выходим за границы массива
         return *this;
     }
     String& operator+=(const char s) {
@@ -158,16 +160,20 @@ public:
     }
     size_t find(const String& substring) const{
         for (size_t i = 0; i < sz - substring.sz;++i) {
-            bool flag = true;//elements_are_same лучше вместо flag
+            bool elements_are_same = true;
             for (size_t j = i; j < substring.sz + i; ++j) {
-                if (str[j] != substring.str[j - i]) flag = false;// А зачем дальше сравнивать, можно сразу break делать если всё плохо
+                if (str[j] != substring.str[j - i]) {
+                    elements_are_same = false;
+                    break;
+                }
             }
-            if (flag) return i;
+            if (elements_are_same) return i;
         }
         return sz;
     }
     size_t rfind(const String& substring) const{
         String reversesub(substring.sz);// зачем делать копии, если можно сделать аналогично find, только с другой индексацией?
+        // разве копипаста в коде - это хорошо?... :(
         String reverse(sz);
         for (size_t i = substring.sz; i > 0; --i) {
             reversesub[substring.sz - i] = substring[i - 1];
