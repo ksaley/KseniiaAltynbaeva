e9c454d2d1462c021a176c6a23408dac79947c69
KseniiaAltynbaeva <70897569+KseniiaAltynbaeva@users.noreply.github.com>
2021-04-05 17:11:08 +0300

    Update hashTable.cpp
    добавила вывод из main'а, деструктор

--- hashTable.cpp
+++ hashTable.cpp
@@ -4,26 +4,26 @@
 #include <random>
 #include <ctime>
 #pragma GCC optimize("Ofast")
- 
+
 struct node {
     std::string key;
     std::string value;
     node* prev;
     node(const std::string& s, const std::string& value, node* prev):key(s),value(value), prev(prev) {}
 };
- 
+
 struct list{
     node* last = nullptr;
     void push(const std::string& key, const std::string& value) {
         node* current = find(key);
         if(!current) {
-            node *next = new node(key, value, last); // не освобождается память для всего что не удалилось, как и в предыдущих задачах
+            node *next = new node(key, value, last);
             last = next;
         }
         else {
             current->value = value;
         }
- 
+
     }
     void extract(const std::string& key) {
         node* current = last;
@@ -33,18 +33,17 @@ struct list{
             current = current->prev;
         }
         if(current) {
-           if (prev) prev->prev = current->prev;
-           else last = current->prev;
-           delete current;
+            if (prev) prev->prev = current->prev;
+            else last = current->prev;
+            delete current;
         }
     }
- 
-    void get(const std::string& key) {
+
+    std::string get(const std::string& key) {
         node* current = find(key);
-        if(current) std::cout << current->value << std::endl; // результат выводится не из main
-        else std::cout << "none" << std::endl;
+        return current?current->value:"none";
     }
- 
+
     node* find(const std::string& key) {
         node* current = last;
         while(current != nullptr && key != current->key) {
@@ -52,19 +51,26 @@ struct list{
         }
         return current;
     }
+    ~list() {
+        while (last) {
+            node* copy = last;
+            last = last->prev;
+            delete copy;
+        }
+    }
 };
- 
+
 class Hash_table {
 private:
     static const size_t hash_table_size = 60000001ull;
     std::vector<list> buckets;
-    const unsigned long long prime =  1e8 + 7;//65610001ull;
+    const unsigned long long prime =  1e8 + 7;
     unsigned long long a, b;
- 
+
     unsigned long long hash_function(const std::string& variable_name) {
         return ((a * string_to_int(variable_name) + b) % prime % hash_table_size);
     }
- 
+
     unsigned long long string_to_int(const std::string& variable_name) {
         unsigned long long sum = 0;
         unsigned long long x = 1;
@@ -76,7 +82,7 @@ private:
         }
         return sum % prime;
     }
- 
+
 public:
     Hash_table(): buckets(hash_table_size){
         std::mt19937 engine;
@@ -91,11 +97,11 @@ public:
     void extract(const std::string& variable_name) {
         buckets[hash_function(variable_name)].extract(variable_name);
     }
-    void get(const std::string& variable_name) {
-        buckets[hash_function(variable_name)].get(variable_name);
+    std::string get(const std::string& variable_name) {
+        return buckets[hash_function(variable_name)].get(variable_name);
     }
 };
- 
+
 int main() {
     freopen("map.in", "r", stdin);
     freopen("map.out", "w", stdout);
@@ -112,11 +118,11 @@ int main() {
             std::cin >> y;
             hashTable.insert(x,y);
         }
-        if (s == "delete") { // лучше сделать else if, чтобы лишний раз не сравнивать строки
+        else if (s == "delete") {
             hashTable.extract(x);
         }
-        if ( s == "get") {
-            hashTable.get(x);
+        else {
+            std::cout << hashTable.get(x) << std::endl;
         }
     }
     return 0;
