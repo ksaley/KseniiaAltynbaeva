ce7e096df53dd957642c2df24c5ffe84fa1c0b04
KseniiaAltynbaeva <70897569+KseniiaAltynbaeva@users.noreply.github.com>
2021-04-05 16:52:09 +0300

    случайно залила не туда...

--- zadachkaA.cpp
+++ zadachkaA.cpp
@@ -1,92 +1,80 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <random>
-#include <ctime>
-#pragma GCC optimize("Ofast")
 
-struct node {
-    std::string variable_name;
-    int value;
-    node* prev;
-    node(const std::string& s, int value, node* prev):variable_name(s),value(value), prev(prev) {}
+
+struct Node{
+	int value;
+	Node* previous;
+	Node() : value(0), previous(nullptr) {}
 };
 
-struct list{
-    node* last = nullptr;
-    int push(const std::string& variable_name, int add) {
-        node* current = find(variable_name);
-        if(!current) {
-            node *next = new node(variable_name, add, last);
-            last = next;
-            return next->value;
-        }
-        else {
-            current->value += add;
-            return current->value;
-        }
-    }
-    node* find(const std::string& variable_name) {
-        node* current = last;
-        while(current != nullptr && variable_name != current->variable_name) {
-            current = current->prev;
-        }
-        return current;
-    }
-    ~list() {
-        while (last) {
-            node* copy = last;
-            last = last->prev;
-            delete copy;
-        }
-    }
-};
-
-class Hash_table {
+class Stack {
 private:
-    static const size_t hash_table_size = 60000001ull;
-    std::vector<list> buckets;
-    const unsigned long long prime =  1e8 + 7;
-    unsigned long long a, b;
-
-    unsigned long long hash_function(const std::string& variable_name) {
-        return ((a * string_to_int(variable_name) + b) % prime % hash_table_size);
-    }
-
-    unsigned long long string_to_int(const std::string& variable_name) {
-        unsigned long long sum = 0;
-        unsigned long long x = 1;
-        const unsigned long long deg = 997;
-        for(size_t i = 0; i < variable_name.size(); ++i) {
-            sum = (sum + variable_name[i] * x);
-            x *= deg;
-            x %= prime;
-        }
-        return sum % prime;
-    }
-
+	size_t size;
+	Node* last;
 public:
-    Hash_table(): buckets(hash_table_size){
-        std::mt19937 engine;
-        engine.seed(std::time(nullptr));
-        a = engine() % hash_table_size;
-        b = engine() % hash_table_size;
-    }
-    int insert(const std::string& variable_name, int add) {
-        return buckets[hash_function(variable_name)].push(variable_name,add);
-    }
+	Stack() : size(0), last(nullptr) {}
+	
+	size_t length() {
+		return size;
+	}
+	
+	void push(int x) {
+		Node* next = new Node();
+		next->value = x;
+		next->previous = last;
+		++size;
+		last = next;
+	}
+	
+	int back() {
+		return last->value;
+	}
+	
+	int pop() {
+		int result = last->value;
+		Node* copy = last;
+		last = last->previous;
+		delete copy;
+		--size;
+		return result;
+	}
+	
+	void clear() {
+		while (size > 0) {
+			pop();
+		}
+	}
 };
-
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    Hash_table hashTable;
-    std::string s;
-    while(std::cin >> s) {
-        int n;
-        std::cin >> n;
-        std::cout << hashTable.insert(s,n) << std::endl;
-    }
-    return 0;
+	std::string operation;
+	std::cin >> operation;
+	Stack stack;
+	while (operation != "exit") {
+		if (operation == "push") {
+			int n;
+			std::cin >> n;
+			stack.push(n);
+			std::cout << "ok" << '\n';
+		}
+		if (operation == "pop") {
+			if (stack.length() > 0) std::cout << stack.pop() << '\n';
+			else std::cout << "error" << '\n';
+		}
+		if (operation == "back") {
+			if (stack.length() > 0) std::cout << stack.back() << '\n';
+			else  std::cout << "error" << '\n';
+
+		}
+		if (operation == "size") {
+			std::cout << stack.length() << '\n';
+		}
+		if (operation == "clear") {
+			stack.clear();
+			std::cout << "ok" << '\n';
+		}
+	std::cin >> operation;
+ 	}
+	std::cout << "bye" << '\n';
+	return 0;
 }
